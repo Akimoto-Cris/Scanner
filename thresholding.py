42fba2222cb815fe25ac9f2e266ae0fcf8734016
Akimoto-Cris <akimotohisashibi@outlook.com>
2018-08-30 00:03:41 +0800

    Add files via upload

--- thresholding.py
+++ thresholding.py
@@ -1,4 +1,6 @@
-import cv2, argparse
+from cv2 import imread, imwrite, equalizeHist, adaptiveThreshold, waitKey
+from cv2 import resize, imshow, IMREAD_GRAYSCALE, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY
+import argparse
 
 
 def get_args():
@@ -6,18 +8,18 @@ def get_args():
     parser.add_argument("-i", "--image_path", type=str, help="path to the image")
     parser.add_argument("-d", "--display", type=int, default=1,
                         help="whether showing the image after processing. (1 / 0). default is true. (optional)")
-    parser.add_argument("-c", "--downsample", type=float, default=1,
+    parser.add_argument("-c", "--downsample", type=float,
                         help="the factor you want to shrink resolution of output. (optional)")
     return parser.parse_args()
 
 
 def histeq(pic_path):
-    image = cv2.imread(pic_path, cv2.IMREAD_GRAYSCALE)
-    return cv2.equalizeHist(image)
+    image = imread(pic_path, IMREAD_GRAYSCALE)
+    return equalizeHist(image)
 
 
 def adaptive_thresholding(image, block_size, C):
-    return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, block_size, C)
+    return adaptiveThreshold(image, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, block_size, C)
 
 
 if __name__ == "__main__":
@@ -25,10 +27,10 @@ if __name__ == "__main__":
     histed_image = histeq(args.image_path)
     thresed_image = adaptive_thresholding(histed_image, 35, 20)
     if args.display:
-        cv2.imshow("thresed_image", thresed_image)
-        cv2.waitKey()
+        imshow("thresed_image", thresed_image)
+        waitKey()
 
     if args.downsample:
-        thresed_image = cv2.resize(thresed_image, (int(thresed_image.shape[1] // args.downsample),
+        thresed_image = resize(thresed_image, (int(thresed_image.shape[1] // args.downsample),
                                    int(thresed_image.shape[0] // args.downsample)))
-    cv2.imwrite("{}_1.jpg".format(args.image_path.split('.')[0]), thresed_image)
+    imwrite("{}_1.jpg".format(args.image_path.split('.')[0]), thresed_image)
